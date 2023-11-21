918818706851a9cdb102b3af0d1b9dbc8a7fc2a8
sahelhoseini <55993734+sahelhoseini@users.noreply.github.com>
2023-11-21 20:05:11 +0330

    Update urls.py

--- sephora/shopping/urls.py
+++ sephora/shopping/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns=[
     path('productlist/', views.productlist , name='productlist'),
+    path('productitem/', views.productitem , name='productitem'),
     path('', views.home , name='home'),
 ]
 
