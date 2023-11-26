bb44e471700823d622e44792270d7c3c35735931
sahelhoseini <55993734+sahelhoseini@users.noreply.github.com>
2023-11-26 22:14:02 +0330

    add contactapp's url to project

--- sephora/sephora/urls.py
+++ sephora/sephora/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shopping.urls')),
+    path('contact/', include('contactapp.urls')),
 ]
 
 
