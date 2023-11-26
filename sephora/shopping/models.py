ee6374e77ff89b769c8c6848229c2723817beb94
sahelhoseini <55993734+sahelhoseini@users.noreply.github.com>
2023-11-26 23:02:23 +0330

    add price_off to model

--- sephora/shopping/models.py
+++ sephora/shopping/models.py
@@ -7,6 +7,7 @@ class Products(models.Model):
     title=models.CharField(max_length=200, verbose_name='عنوان')
     description=models.TextField(verbose_name='توضیحات')
     price=models.IntegerField(verbose_name='قیمت')
+    price_off=models.IntegerField(verbose_name='قیمت تخقیف',null=True)
     ratting=models.IntegerField(default=0, verbose_name='امتیاز')
     slug=models.SlugField(null=True,blank=True,unique=True)
     code=models.CharField(max_length=7, null=True, verbose_name='کد کالا')
