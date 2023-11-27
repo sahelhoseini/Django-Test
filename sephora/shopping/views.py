57bf0c7db674f402e240fd48e834513e9d5e99cd
sahelhoseini <55993734+sahelhoseini@users.noreply.github.com>
2023-11-27 09:05:47 +0330

    Update views.py

--- sephora/shopping/views.py
+++ sephora/shopping/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,HttpResponse
-from  .models import Products
+from  .models import Products,Imageofslider
 # Create your views here.
 
 
 def home(request):
+    slider=Imageofslider.objects.all().order_by('-created')
+    slider=slider[:4]
     product=Products.objects.all().order_by('-created')
-    return render(request,'home.html',{'product':product })
+    product=product[:4]
+    return render(request,'home.html',{'product':product ,'slider':slider})
 
 def productlist(request):
     all_product=Products.objects.all().order_by('-created')
