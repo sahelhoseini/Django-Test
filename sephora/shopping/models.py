6dcfc191b462053c1bac3ff699302f4117361651
sahelhoseini <55993734+sahelhoseini@users.noreply.github.com>
2023-11-27 09:05:30 +0330

    add new model

--- sephora/shopping/models.py
+++ sephora/shopping/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
+from autoslug import AutoSlugField
 
 # Create your models here.
 
@@ -8,16 +10,34 @@ class Products(models.Model):
     description=models.TextField(verbose_name='توضیحات')
     price=models.IntegerField(verbose_name='قیمت')
     price_off=models.IntegerField(verbose_name='قیمت تخقیف',null=True)
-    ratting=models.IntegerField(default=0, verbose_name='امتیاز')
-    slug=models.SlugField(null=True,blank=True,unique=True)
+    ratting=models.IntegerField(default=1, verbose_name='امتیاز',validators=[MinValueValidator(1),MaxValueValidator(5)])
+    slug= AutoSlugField(populate_from='title',editable=False, always_update=True)
     code=models.CharField(max_length=7, null=True, verbose_name='کد کالا')
     created=models.DateField(auto_now_add=True,verbose_name='تاریخ ایجاد', null=True)
     image=models.ImageField(upload_to='products',null=True)
 
 
+
     class Meta:
         verbose_name = 'محصول'
         verbose_name_plural = 'محصولات'
+
+    def __str__(self):
+        return self.title
+
+
+
+class Imageofslider(models.Model):
+    title=models.CharField(max_length=100,verbose_name='عنوان')
+    short_description=models.CharField(max_length=150,verbose_name='توضیح کوتاه')
+    description=models.TextField(verbose_name='توضیح ')
+    image=models.ImageField(upload_to='slider')
+    created=models.DateField(auto_now_add=True,verbose_name='تاریخ ایجاد', null=True)
+
+    
+    class Meta:
+        verbose_name = 'عکس اسلایدر'
+        verbose_name_plural = 'اسلایدر'
 
     def __str__(self):
         return self.title
